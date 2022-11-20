c6a8014b62fc0a40df5d67df4d941c4876cfbc5f
pundirpankaj78 <pundirpankaj78@gmail.com>
2022-11-20 09:52:24 +0530

    fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
+  //This is done  by code1
   return (
     <div className="header">
       <Link to="/" className="title">
